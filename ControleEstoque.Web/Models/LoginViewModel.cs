175300fc9bdf59090b19abf882adf20031544355
glsantos <gabriel._.lima@hotmail.com>
2018-01-29 13:03:06 -0200

    Estrutura do Projeto(100%) e Login(60%)

---
+++ ControleEstoque.Web/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ControleEstoque.Web.Models {
+    public class LoginViewModel {
+
+        [Required]
+        [Display(Name="Usuário:")]
+        public string Usuario { get; set; }
+
+        [Required
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha:")]
+        public string Senha { get; set; }
+
+        [Display(Name = "Lembrar-me")]
+        public bool LembrarMe { get; set; }
+    }
+}
